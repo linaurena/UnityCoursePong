5ae55079e6b985c391f93ee102a682842d2f6758
Lina María Ureña Bolívar <lina8maria8@hotmail.com>
2021-02-18 15:19:02 -0500

    Uploading scripts

---
+++ DeadZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour
+{
+    public Text scorePlayerText;
+    public Text scoreEnemyText;
+
+    int scorePlayerQuantity;
+    int scoreEnemyQuantity;
+    public SceneChanger sceneChanger;
+
+    private void OnTriggerEnter2D(Collider2D ball)
+    {
+        //Debug.Log("Trigger");
+        if(gameObject.tag == "Left")
+        {
+            scoreEnemyQuantity++;
+            UpdateScoreLabel(scoreEnemyText, scoreEnemyQuantity);
+        }else if(gameObject.CompareTag("Right"))
+        {
+            scorePlayerQuantity++;
+            UpdateScoreLabel(scorePlayerText, scorePlayerQuantity);
+        }
+
+        ball.GetComponent<BallBehaviour>().gameStarted = false;
+        CheckScore();
+    }
+
+    void CheckScore()
+    {
+        if (scorePlayerQuantity >= 3)
+        {
+            sceneChanger.ChangeSceneTo("WinScene");
+        } else if (scoreEnemyQuantity >= 3)
+        {
+            sceneChanger.ChangeSceneTo("LoseScene");
+        }
+    }
+
+    void UpdateScoreLabel(Text label, int score)
+    {
+        label.text = score.ToString();
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePos.y, -3.8f,3.8f), transform.position.z);
+    }
+}
